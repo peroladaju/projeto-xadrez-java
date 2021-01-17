9cb1cc42d01c091a2e64d01024db51c534291592
Juliana Gonçalves <peroladaju@gmail.com>
2021-01-16 21:27:35 -0300

    Primeira class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Projeto de xadez");
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 	}
 
 }
