b973f1a7970a7559dbfd220c03f1df165a9c18f4
Juliana Gonçalves <peroladaju@gmail.com>
2021-01-31 00:59:55 -0200

    Movimentos possíveis: Torre

--- src/xadrez/pecas/Torre.java
+++ src/xadrez/pecas/Torre.java
@@ -1,6 +1,7 @@
 package xadrez.pecas;
 
 import tabuleiro.Board;
+import tabuleiro.Posicao;
 import xadrez.Cor;
 import xadrez.XadrezPeca;
 
@@ -15,4 +16,54 @@ public class Torre extends XadrezPeca{
 	 public String toString() {
 		return "T";
 	}
+
+	@Override
+	public boolean[][] movimentoPossivel() {
+		boolean[][] mat = new boolean[getBoard().getLinhas()][getBoard().getColunas()];
+		Posicao p = new Posicao(0, 0);
+		//Acima
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna());
+		while(getBoard().posicaoExiste(p) && !getBoard().existePeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() - 1);
+		}
+		if(getBoard().posicaoExiste(p) && existePecaOponente(p)) {
+		mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Esquerda
+		p.setValores(posicao.getLinha(), posicao.getColuna() - 1);
+		while(getBoard().posicaoExiste(p) && !getBoard().existePeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() - 1);
+		}
+		if(getBoard().posicaoExiste(p) && existePecaOponente(p)) {
+		mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Para baixo
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna());
+		while(getBoard().posicaoExiste(p) && !getBoard().existePeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() + 1);
+		}
+		if(getBoard().posicaoExiste(p) && existePecaOponente(p)) {
+		mat[p.getLinha()][p.getColuna()] = true;
+		}	
+		
+		//Direita
+		p.setValores(posicao.getLinha(), posicao.getColuna() + 1);
+		while(getBoard().posicaoExiste(p) && !getBoard().existePeca(p)) {
+		mat[p.getLinha()][p.getColuna()] = true;
+		p.setColuna(p.getColuna() + 1);
+		}
+		if(getBoard().posicaoExiste(p) && existePecaOponente(p)) {
+		mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		return mat;
+		
+		
+	}
+	
 }
